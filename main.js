215f364175c9db3bbe7df646e27b41aa5c5d31a6
victorkauan <victorkauan.alu.lmb@gmail.com>
2021-06-25 10:22:41 -0300

    Show and hide mobile Menu

---
+++ main.js
@@ -0,0 +1,18 @@
+// Open and close the menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// Hides the menu when clicking an item
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
