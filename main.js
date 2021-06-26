450f0bacf7d6205cb9d3b1cff25453904c0f287e
victorkauan <victorkauan.alu.lmb@gmail.com>
2021-06-26 19:44:08 -0300

    Back to top button and main.js update

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ for (const link of links) {
   })
 }
 
-// Change the page header when scrolling
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+// Change the page header when scroll
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     // Scroll is greater than header height
     header.classList.add('scroll')
@@ -29,7 +29,7 @@ window.addEventListener('scroll', function () {
     // Scroll is lower than header height
     header.classList.remove('scroll')
   }
-})
+}
 
 // Testimonials carousel/slider (Swiper)
 const swiper = new Swiper('.swiper-container', {
@@ -56,9 +56,29 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links
+#contact .text, #contact .links,
+footer .brand, footer .social
 `,
   {
     interval: 100
   }
 )
+
+// Back to top
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+  })
+}
+
+// When scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
