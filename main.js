f17f9947d85896cd11cebdd3b67cda9e0b1aa8ee
victorkauan <victorkauan.alu.lmb@gmail.com>
2021-06-27 16:57:34 -0300

    Section names become active in the navigation bar according to the current section

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ for (const element of toggle) {
   })
 }
 
-// Hides the menu when clicking an item
+// Hide the menu when clicking an item
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
@@ -18,10 +18,10 @@ for (const link of links) {
 }
 
 // Change the page header when scroll
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     // Scroll is greater than header height
     header.classList.add('scroll')
@@ -43,7 +43,7 @@ const swiper = new Swiper('.swiper-container', {
   breakpoints: {
     767: {
       slidesPerView: 2,
-      setWrapperSize: true // Sets the wrapper size to fixed, thus always showing the defined amount of slides
+      setWrapperSize: true // Set the wrapper size to fixed, thus always showing the defined amount of slides
     }
   }
 })
@@ -71,9 +71,9 @@ footer .brand, footer .social
 )
 
 // Back to top
-function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
+const backToTopButton = document.querySelector('.back-to-top')
 
+function backToTop() {
   window.addEventListener('scroll', function () {
     if (window.scrollY >= 560) {
       backToTopButton.classList.add('show')
@@ -83,8 +83,35 @@ function backToTop() {
   })
 }
 
+// Activate the menu according to the visible section on the page
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + window.innerHeight / 2
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 // When scroll
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
